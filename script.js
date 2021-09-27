1248d233106b24e0d1eaeeb6e1412f7776810b53
MITHIRI1 <87658049+MITHIRI1@users.noreply.github.com>
2021-09-26 21:19:12 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+/*ES2015 Global Constants
+Write an ES2015 Version */
+
+const PI = 3.14
+PI = 42
+
+/*Quiz
+
+What is the difference between var and let?
+The difference is var function scoped and let is blocked scoped.
+
+What is the difference between var and const?
+The main difference between them is var is function scoped and cosnt is block scoped.
+
+What is the difference between let and const?
+Const is a signal identifer that can be reassigned and let is a signal identifer that can be reassigned. 
+
+What is hoisting?
+Is moving the declaration at top of the scope before the code executes. 
+*/
+
